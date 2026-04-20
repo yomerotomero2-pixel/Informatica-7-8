216e368e8c9615775df07dd61f5def213ced7abd
yomerotomero2-pixel <yomerotomero2@gmail.com>
2026-04-20 17:26:25 +0000

    aco

---
+++ js-events/script.js
@@ -0,0 +1,22 @@
+//function addNumbers(num1, num2){
+ //        let sum = num1 + num2;
+  //   alert("Sum is equal to" + sum);
+
+ //}
+
+//addNumbers(6,7);
+
+function printArticle1(){
+     alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.");
+
+}
+
+function printArticle2(){
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.")
+
+}
+
+function printArticle3(){
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.")
+
+}
