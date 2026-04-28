2f24904db18eec738aca1420a7e2b8bcbd2d6d73
yomerotomero2-pixel <yomerotomero2@gmail.com>
2026-04-28 16:12:10 +0000

    coin

---
+++ js-coin/script.js
@@ -0,0 +1,13 @@
+function coin(){
+    let randumNum = Math.random(); //aqui guaradamos este numero en una variable que podemos usar luego
+    console.log(randumNum); //esto solo es para ver los numero que se estan generando
+
+    let subtitle = document.querySelector("h2"); //con el queryselector agarramos cosas del html para usarlos rn el script.js
+    if(randumNum > 0.5){
+        subtitle.innerHTML = "Heads"; //de mi subtitile vamos a modificar el texto
+
+    }else{
+        subtitle.innerHTML ="Tails";
+    }
+}
+
